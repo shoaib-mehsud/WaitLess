32e561d02b32177450f3a8e0ba900516a8534450
shoaib-mehsud <shoaibkhanmehsud41@gmail.com>
2026-02-25 23:50:17 +0500

    feat(database): initialize multi-tenant schema and Prisma ORM
    - Set up PostgreSQL connection and Prisma configuration
    - Map User, Business, Queue, and Token models with strict relations
    - Implement state machine Enums for Queue and Token lifecycles
    - Establish folder Structure

---
+++ prisma/migrations/20260225174116_init_db/migration.sql
@@ -0,0 +1,101 @@
+-- CreateEnum
+CREATE TYPE "UserStatus" AS ENUM ('PENDING', 'ACTIVE', 'SUSPENDED');
+
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('SUPER_ADMIN', 'BUSINESS_ADMIN', 'STAFF', 'CUSTOMER');
+
+-- CreateEnum
+CREATE TYPE "BusinesStatus" AS ENUM ('PENDING', 'APPROVED', 'REJECTED', 'SUSPENDED');
+
+-- CreateEnum
+CREATE TYPE "QueueStatus" AS ENUM ('OPEN', 'PAUSE', 'LOCKED', 'CLOSED');
+
+-- CreateEnum
+CREATE TYPE "TokenState" AS ENUM ('WAITING', 'CALLED', 'SERVING', 'COMPLETED', 'SKIPPED', 'PRIORITY', 'CANCELLED', 'EXPIRED');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+    "email" VARCHAR(100) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "userRole" "Role" NOT NULL DEFAULT 'CUSTOMER',
+    "status" "UserStatus" NOT NULL DEFAULT 'PENDING',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "businesses" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+    "status" "BusinesStatus" NOT NULL DEFAULT 'PENDING',
+    "ownerId" INTEGER NOT NULL,
+    "createdAT" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "businesses_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "queues" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(40) NOT NULL,
+    "businessId" INTEGER NOT NULL,
+    "defaultServiceTime" INTEGER NOT NULL,
+    "lastPausedAt" TIMESTAMPTZ,
+    "totalPausedTime" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMPTZ NOT NULL,
+
+    CONSTRAINT "queues_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "queue_manager" (
+    "userId" INTEGER NOT NULL,
+    "queueId" INTEGER NOT NULL,
+
+    CONSTRAINT "queue_manager_pkey" PRIMARY KEY ("userId","queueId")
+);
+
+-- CreateTable
+CREATE TABLE "tokens" (
+    "id" SERIAL NOT NULL,
+    "queueId" INTEGER NOT NULL,
+    "businessId" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "sequenceNumber" INTEGER NOT NULL,
+    "state" "TokenState" NOT NULL DEFAULT 'WAITING',
+    "calledAt" TIMESTAMPTZ,
+    "servedAt" TIMESTAMPTZ,
+    "completedAt" TIMESTAMPTZ,
+    "createdAt" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMPTZ NOT NULL,
+
+    CONSTRAINT "tokens_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "businesses" ADD CONSTRAINT "businesses_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "queues" ADD CONSTRAINT "queues_businessId_fkey" FOREIGN KEY ("businessId") REFERENCES "businesses"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "queue_manager" ADD CONSTRAINT "queue_manager_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "queue_manager" ADD CONSTRAINT "queue_manager_queueId_fkey" FOREIGN KEY ("queueId") REFERENCES "queues"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "tokens" ADD CONSTRAINT "tokens_queueId_fkey" FOREIGN KEY ("queueId") REFERENCES "queues"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "tokens" ADD CONSTRAINT "tokens_businessId_fkey" FOREIGN KEY ("businessId") REFERENCES "businesses"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "tokens" ADD CONSTRAINT "tokens_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
